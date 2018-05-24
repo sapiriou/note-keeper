67d8f1ac0e3794a51c224b4ea4abdbd5210a6c7c
Daniel Garnier-Moiroux <dgarnier-moiroux@pivotal.io>
2018-05-24 17:18:54 +0200

    beautify note contents (display words !)
    Signed-off-by: Sandrine Piriou <spiriou@pivotal.io>

--- app/src/main/java/io/pivotal/notekeeper/note/NoteService.kt
+++ app/src/main/java/io/pivotal/notekeeper/note/NoteService.kt
@@ -5,7 +5,7 @@ import java.util.*
 class NoteService private constructor() {
 
 
-    private val zuperText =
+    private val sampleText =
 """Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sed leo in augue suscipit euismod. Sed finibus tincidunt lectus, eu dignissim nunc dapibus et. Donec egestas sapien varius fringilla consequat. Sed eget augue leo. Sed facilisis finibus purus quis fringilla. Morbi at mi eu nibh tincidunt ultrices. Aliquam id nisi nec ipsum suscipit mattis vel id nisl. Aliquam in vulputate augue, sit amet eleifend justo. Etiam mi massa, consequat quis aliquet quis, rutrum sit amet enim. Proin id augue at massa suscipit sodales eu in diam. Mauris cursus, augue in suscipit finibus, ex dolor lacinia odio, eget fringilla tortor quam quis elit. Nulla imperdiet nibh ex, a tincidunt odio ullamcorper ac. Aenean metus erat, malesuada a arcu eget, scelerisque laoreet libero.
 
 In tincidunt, urna eget scelerisque ullamcorper, tellus arcu ultrices magna, malesuada lacinia ante elit condimentum neque. Nam finibus leo at ultricies vulputate. Nam ac leo sollicitudin, varius ligula a, gravida elit. Suspendisse lorem enim, dignissim et egestas sit amet, elementum pellentesque nisl. Cras commodo tempus odio, vel egestas nunc ultrices quis. Aliquam imperdiet magna facilisis, dictum risus ac, vehicula tortor. Mauris blandit dolor felis, eu congue turpis volutpat vitae. Fusce consectetur commodo leo in sodales. Maecenas vulputate lobortis nisi porta dictum. Nulla in pretium justo, sed congue leo. Maecenas eleifend consectetur libero a vulputate. Etiam ac erat fermentum, volutpat turpis sit amet, finibus nisl. Integer consequat sapien eget turpis dapibus feugiat. Duis maximus justo metus, vel fringilla enim cursus vel.
@@ -18,10 +18,13 @@ Praesent quis turpis quam. Nulla ultricies bibendum mi, quis tempus arcu varius 
 
 """
 
-    // TODO: use makeSuperContent for text
-    val notes: List<Note> = (0..9).map { Note(it, "Title $it", zuperText) }
+    val notes: List<Note> = (0..19).map { Note(it, "Title $it", makeRandomNoteContent()) }
 
-    private fun makeSuperContent() = zuperText.substring((Random().nextInt(zuperText.length / 2)..200))
+    private fun makeRandomNoteContent(): String {
+        val words = sampleText.split(Regex("\\s"))
+        val startingPoint = Random().nextInt(words.size / 2)
+        return words.drop(startingPoint).take(100).joinToString(" ").capitalize()
+    }
 
     companion object {
         val instance: NoteService by lazy { NoteService() }
